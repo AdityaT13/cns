fe1d3485ed9546c16b8cbe586ced6b80623fdffc
Aditya Teekinavar <145801223+AdityaT13@users.noreply.github.com>
2025-12-29 01:13:26 +0530

    Add files via upload

---
+++ vigFILE.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void encryptFile();
+void decryptFile();
+
+int main()
+{
+    int ch;
+    while (1)
+    {
+        printf("\n1. Encrypt\n2. Decrypt\n3. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &ch);
+
+        if (ch == 1)
+            encryptFile();
+        else if (ch == 2)
+            decryptFile();
+        else if (ch == 3)
+            exit(0);
+        else
+            printf("Invalid choice\n");
+    }
+}
+
+void encryptFile()
+{
+    FILE *fin, *fout;
+    char key[50], ch;
+    char infile[50], outfile[50];
+    int i, klen, shift;
+
+    printf("Enter input file name: ");
+    scanf("%s", infile);
+
+    printf("Enter output file name: ");
+    scanf("%s", outfile);
+
+    fin = fopen(infile, "r");
+    if (fin == NULL)
+    {
+        printf("File error\n");
+        return;
+    }
+
+    fout = fopen(outfile, "w");
+    if (fout == NULL)
+    {
+        printf("File error\n");
+        fclose(fin);
+        return;
+    }
+
+    printf("Enter KEY: ");
+    scanf("%s", key);
+
+    klen = strlen(key);
+    if (klen == 0)
+    {
+        printf("Invalid key value\n");
+        fclose(fin);
+        fclose(fout);
+        return;
+    }
+
+    for (i = 0; i < klen; i++)
+    {
+        if (key[i] >= 'a' && key[i] <= 'z')
+            key[i] = key[i] - 32;
+
+        if (key[i] < 'A' || key[i] > 'Z')
+        {
+            printf("Invalid key value\n");
+            fclose(fin);
+            fclose(fout);
+            return;
+        }
+    }
+
+    i = 0;
+    while ((ch = fgetc(fin)) != EOF)
+    {
+        if (ch >= 'A' && ch <= 'Z')
+        {
+            shift = key[i % klen] - 'A';
+            ch = (ch - 'A' + shift) % 26 + 'A';
+            i++;
+        }
+        else if (ch >= 'a' && ch <= 'z')
+        {
+            shift = key[i % klen] - 'A';
+            ch = (ch - 'a' + shift) % 26 + 'a';
+            i++;
+        }
+        fputc(ch, fout);
+    }
+
+    fclose(fin);
+    fclose(fout);
+    printf("Encryption done\n");
+}
+
+void decryptFile()
+{
+    FILE *fin, *fout;
+    char key[50], ch;
+    char infile[50], outfile[50];
+    int i, klen, shift;
+
+    printf("Enter encrypted file name: ");
+    scanf("%s", infile);
+
+    printf("Enter decrypted file name: ");
+    scanf("%s", outfile);
+
+    fin = fopen(infile, "r");
+    if (fin == NULL)
+    {
+        printf("Encryption not done\n");
+        return;
+    }
+
+    fout = fopen(outfile, "w");
+    if (fout == NULL)
+    {
+        printf("File error\n");
+        fclose(fin);
+        return;
+    }
+
+    printf("Enter SAME KEY: ");
+    scanf("%s", key);
+
+    klen = strlen(key);
+    if (klen == 0)
+    {
+        printf("Invalid key value\n");
+        fclose(fin);
+        fclose(fout);
+        return;
+    }
+
+    for (i = 0; i < klen; i++)
+    {
+        if (key[i] >= 'a' && key[i] <= 'z')
+            key[i] = key[i] - 32;
+
+        if (key[i] < 'A' || key[i] > 'Z')
+        {
+            printf("Invalid key value\n");
+            fclose(fin);
+            fclose(fout);
+            return;
+        }
+    }
+
+    i = 0;
+    while ((ch = fgetc(fin)) != EOF)
+    {
+        if (ch >= 'A' && ch <= 'Z')
+        {
+            shift = key[i % klen] - 'A';
+            ch = (ch - 'A' - shift + 26) % 26 + 'A';
+            i++;
+        }
+        else if (ch >= 'a' && ch <= 'z')
+        {
+            shift = key[i % klen] - 'A';
+            ch = (ch - 'a' - shift + 26) % 26 + 'a';
+            i++;
+        }
+        fputc(ch, fout);
+    }
+
+    fclose(fin);
+    fclose(fout);
+    printf("Decryption done\n");
+}
